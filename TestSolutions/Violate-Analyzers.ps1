4bd0da568f51b21502c21a19424c3fa0b8ea64e3
Benedek Farkas <benedek.farkas@lombiq.com>
2024-06-14 16:55:42 +0200

    Updating Violate-Analyzers to test warnings in Measure-VariableNameCasing

--- TestSolutions/Violate-Analyzers.ps1
+++ TestSolutions/Violate-Analyzers.ps1
@@ -1,7 +1,8 @@
-function Violate-Analyzers()
+function Violate-Analyzers($parameter = $LastExitCode)
 {
-    if (!$false) {
-        Write-Host 'This file contains intentionally bad code to verify that PSScriptAnalyzer works correctly.'
+    $Message = 'This file contains intentionally bad code to verify that PSScriptAnalyzer works correctly.'
+    if (!$Parameter) {
+        Write-Host $message
     }
 }
 
